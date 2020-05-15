eb5dd15be16d17246690b58ca267777844056169
pratik patil <pppyt@gmail.com>
2020-05-15 11:38:37 +0530

    changes

--- Website/src/Components/script.js
+++ Website/src/Components/script.js
@@ -1,13 +1,18 @@
 $(function () {
   var $inputfield = $("#email");
   var $otpButton = $("#otp");
+  var $emailError = $(".emailError");
+  var $otpError = $(".otpError");
 
   $("#addEmail").on("click", function () {
     if ($inputfield.val() == "") {
-      alert("Email is required");
+      //Display Email error.
+      $emailError.css({ display: "block" });
     } else {
       //Flip the button.
       $(".vButtonFlipper").addClass("flipped");
+      //Disabling the error message.
+      $emailError.css({ display: "none" });
 
       //Post request for sending email.
       $.ajax({
@@ -49,19 +54,18 @@ $(function () {
           //Checking if the entered otp is correct with the one which we are getting from response.
           if (response.isOtpValid) {
             $otpButton.html("Verifying...");
+            $otpError.css({ display: "none" });
             window.location.assign(
               "http://app.pointtopointonline.com/index.php?/setup/account/" +
                 response.userId
             );
           } else {
-            alert("Incorrect otp. Try again");
+            $otpError.css({ display: "block" });
             $inputfield.val("");
           }
         },
         dataType: "text",
-        error: function () {
-          alert("Error");
-        },
+        error: function () {},
       });
     });
   });
